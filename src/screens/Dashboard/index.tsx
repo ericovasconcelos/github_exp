405c409f8571c76a2d8ba391c8af80d33156671d
Erico Vasconcelos <erico@ettbrasil.com.br>
2022-04-05 13:18:15 -0300

    [ADD] Full test pass

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/core';
-import React, { useRef, useState } from 'react';
+import React, { SyntheticEvent, useRef, useState } from 'react';
 import { TextInput } from 'react-native';
 
 import { Background } from '../../components/Background';
@@ -26,24 +26,18 @@ export function Dashboard() {
 
   const { addRepository, repositories } = useRepositories();
 
+  function handleTextInput(text: string) {
+    setInputText(text);
+  }
+
   function handleAddRepository() {
-    /**
-     * TODO: 
-     * - call addRepository function sending inputText value;
-     * - clean inputText value.
-     */
+    addRepository(inputText);
+    setInputText('');
     inputRef.current?.blur();
   }
 
   function handleRepositoryPageNavigation(id: number) {
-    /**
-     * TODO - navigate to the Repository screen sending repository id.
-     * Remember to use the correct prop name (repositoryId) to the repositoy id:
-     * 
-     * navigate(SCREEN NAME, {
-     *  repositoryId: id of the repository
-     * })
-     */
+    navigate('Repository', {repositoryId: id});
   }
 
   return (
@@ -57,11 +51,7 @@ export function Dashboard() {
               ref={inputRef}
               placeholder="Digite aqui 'usuário/repositório'"
               value={inputText}
-              /**
-               * TODO - update inputText value when input text value 
-               * changes:
-               * onChangeText={YOUR CODE HERE}
-               */
+              onChangeText={handleTextInput}
               onSubmitEditing={handleAddRepository}
               returnKeyType="send"
               autoCapitalize='none'
@@ -71,11 +61,7 @@ export function Dashboard() {
             <InputButton
               testID="input-button"
               onPress={handleAddRepository}
-            /**
-             * TODO - ensure to disable button when inputText is 
-             * empty (use disabled prop to this):
-             * disabled={CONDITION HERE}
-             */
+              disabled={inputText.trim().length === 0? true : false}
             >
               <Icon name="search" size={20} />
             </InputButton>
